3da3a43ba31e57dd316323ec5bc627ad42bc22d7
히정 <134138015+hui-jeong@users.noreply.github.com>
2025-06-18 23:04:58 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.10 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -1,9 +1,6 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        for(int i = my_string.length()-1; i>=0; i--){
-            answer+=my_string.charAt(i);
-        }
-        return answer;
+        StringBuffer sb = new StringBuffer(my_string);
+        return sb.reverse().toString();
     }
 }
