4f830f1561398cf7ff1a55a97186f8ae3bb19a23
히정 <134138015+hui-jeong@users.noreply.github.com>
2025-06-23 21:28:04 +0900

    [level 0] Title: 외계행성의 나이, Time: 3.39 ms, Memory: 81.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -0,0 +1,9 @@
+import java.util.stream.*;
+class Solution {
+    public String solution(int age) {
+        return String.valueOf(age)
+                     .chars()
+                     .mapToObj(c -> String.valueOf((char)('a'+(c - '0'))))
+                     .collect(Collectors.joining());
+    }
+}
